45bda91e224df9e35ad9e81553f47515ee0e8507
Yuri Rebrov <yurirebrov01@gmail.com>
2023-04-16 19:36:40 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E7E4E6',
+        secondary: '#37323E',
+        accent: '#EC9A29',
+        label: '#A3A3A3',
+        logo: '#0F8B8D'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      boxShadow: {
+        'headerDownShadow': '0px 5px 10px rgba(160, 160, 160, 0.25)'
+      }
+    },
   },
   plugins: [],
 }
