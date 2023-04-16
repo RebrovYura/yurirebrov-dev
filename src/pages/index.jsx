b3068ee5cb2c7af431a960ea0bf9235db0929d86
Yuri Rebrov <yurirebrov01@gmail.com>
2023-04-16 19:19:35 +0300

    create basic structure, add tailwind

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Yuri Rebrov | Homepage</title>
+        <meta name="description" content="Yuri's homepage" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        Hello
+      </main>
+    </>
+  )
+}
