e0d059b54438eff615ac424736bad8b93c7775a6
Yuri Rebrov <yurirebrov01@gmail.com>
2023-08-09 12:42:05 +0300

    change link to resume

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
         <p className='text-[32px] md:text-[40px] leading-[30px] font-bold text-center md:text-left'>Hi, I&apos;m <span className='text-accent'>Yuri Rebrov</span></p>
         <p className='text-[14px] md:text-[16px] font-light italic text-center md:text-left my-[13px]'>self-taught, striving for new heights</p>
         <motion.a
-          href='https://drive.google.com/file/d/1qqGTIbSuJpQji_WqvpNSc5UVfULnMXBp/view?usp=sharing'
+          href='https://drive.google.com/file/d/1BoxwCgfUYyXHH6Mbatm7DLl6r94MAsip/view?usp=sharing'
           target='_blank'
           whileTap={{ scale: 0.9 }}
           className='text-[16px] block font-semibold max-w-[480px] text-left border-[2px] dark:border-primary border-label rounded-[10px] p-[10px] cursor-pointer select-none'
